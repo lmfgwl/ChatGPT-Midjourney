abc3bc3f1f61837356b74b5e76af99c6e1f375d0
lmfgwl <31383632+lmfgwl@users.noreply.github.com>
2023-07-11 22:47:39 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,9 +437,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>ChatGPT And MJ </div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              Ruicheng Media旗下GPT
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
