a1a2db3d8eb8950a853dd2f0e0e966cf648e7bfd
lmfgwl <31383632+lmfgwl@users.noreply.github.com>
2023-07-13 00:10:57 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -3,6 +3,7 @@ export const REPO = "ChatGPT-Midjourney";
 export const REPO_URL = `https://moonvy.com/apps/ops`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const OPSCORN_URL = `https://moonvy.com/apps/ops`;
+export const FANYI_URL = `https://fanyi.baidu.com`;
 export const WECHAT_URL = `https://cdn.discordapp.com/attachments/1125708015673286660/1127289305174380615/2.png`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
